2722162927f9f38944a44ad2835c113a871e8709
RoyaleAimz <sycozxhalo@gmail.com>
2025-05-17 05:17:07 -0400

    Add files via upload

---
+++ vantssite/components/BackButton.js
@@ -0,0 +1,1 @@
+export default function BackButton(){return<button onClick={()=>history.back()} className="px-3 py-1 bg-gray-700 hover:bg-gray-600 rounded text-sm">Back</button>;}
---
+++ vantssite/components/PaymentBar.js
@@ -0,0 +1,1 @@
+export default function PaymentBar(){return<div id="payments" className="fixed bottom-0 left-0 w-full bg-gray-800 py-2 flex justify-center space-x-4 z-50">{['mastercard','visa','amex','paypal','apple-pay','google-pay','crypto','cash-app'].map(name=><img key={name} src={`/images/${name}.svg`} alt={name} className="h-6"/>)}</div>;}
